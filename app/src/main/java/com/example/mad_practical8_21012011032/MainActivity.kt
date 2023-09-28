c5bc4ecb63e0eb14351c9969fc451c7f622d6e55
JaviyaShreya <shreyajaviya@gmail.com>
2023-09-28 21:46:26 +0530

    pr8

--- app/src/main/java/com/example/mad_practical8_21012011032/MainActivity.kt
+++ app/src/main/java/com/example/mad_practical8_21012011032/MainActivity.kt
@@ -1,25 +1,61 @@
 package com.example.mad_practical8_21012011032
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.app.TimePickerDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.google.android.material.button.MaterialButton
 import com.google.android.material.card.MaterialCardView
+import java.util.Calendar
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+class MainActivity : AppCompatActivity()
+{
+    override fun onCreate(savedInstanceState: Bundle?)
+    {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val cardView=findViewById<MaterialCardView>(R.id.card2)
-
-        val createAlam=findViewById<MaterialButton>(R.id.button_Alarm)
-        createAlam.setOnClickListener{
+        val cardView = findViewById<MaterialCardView>(R.id.card2)
+        cardView.visibility=View.GONE
+        val createAlarm = findViewById<MaterialButton>(R.id.button_Alarm)
+        createAlarm.setOnClickListener{
             //cardView.visibility=View.VISIBLE
-            TimePickerDialog(this,{tp,hour,minute->setAlarmtime(hour,minute)},11,0,false).show()
+            TimePickerDialog(this, { tp, hour, minute -> setAlarmTime(hour, minute) },Calendar.HOUR, Calendar.MINUTE,false).show()
         }
     }
-    fun setAlarmtime(hour:Int,minute:Int){
+    fun setAlarmTime(hour:Int,minute:Int)
+    {
+        val cardView=findViewById<MaterialCardView>(R.id.card2)
+        cardView.visibility=View.GONE
+        val alarmtime=Calendar.getInstance()
+        val year=alarmtime.get(Calendar.YEAR)
+        val month=alarmtime.get(Calendar.MONTH)
+        val date=alarmtime.get(Calendar.DATE)
+        alarmtime.set(year, month, date, hour, minute)
+        setAlarm(alarmtime.timeInMillis,AlarmBroadcastReceiver.ALARMSTART)
         //cardView.visibility=View.GONE
+    }
+
+
+    fun stop()
+    {
+        setAlarm(-1,AlarmBroadcastReceiver.ALARMSTOP)
+    }
+
+    fun setAlarm(millitime:Long,action:String)
+    {
+        val intentalarm=Intent(applicationContext,AlarmBroadcastReceiver::class.java)
+        intentalarm.putExtra(AlarmBroadcastReceiver.ALARMKEY,action)
+        val pendingintent=PendingIntent.getBroadcast(applicationContext,4356,intentalarm,PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT)
+        val alarmmanager=getSystemService(ALARM_SERVICE) as AlarmManager
+        if(action==AlarmBroadcastReceiver.ALARMSTART){
+            alarmmanager.setExact(AlarmManager.RTC_WAKEUP,millitime,pendingintent)
+        }
+        else if(action==AlarmBroadcastReceiver.ALARMSTOP)
+            alarmmanager.cancel(pendingintent)
+        sendBroadcast(intentalarm)
+
     }
 }
